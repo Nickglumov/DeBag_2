de90fb4533b7c33b8f3aed71953d6bb5443efdf4
Nickglumov <nekitglum@gmail.com>
2025-02-19 17:19:43 +0200

    Add files via upload

---
+++ DeBag 2.py
@@ -0,0 +1,45 @@
+import os
+
+from weather_sdk import get_new_event, SMSServer
+
+token = os.getenv('FORECAST_TOKEN')
+town_title = 'Курск'
+
+token = os.getenv('SMS_TOKEN')
+server = SMSServer(token)
+
+new_event = get_new_event(token, town_title)
+event_date = new_event.get_date()
+event_time = new_event.get_time()
+event_area = new_event.get_area()
+phenomenon_description = new_event.get_phenomenon()
+
+sms_template = '''{town_title}: {event_time} {event_date} {event_area} ожидается {phenomenon_description}. Будьте внимательны и осторожны.'''
+
+sms_message = sms_template.format(
+    phenomenon_description,
+    town_title,
+    event_time,
+    event_date,
+    event_area,
+)
+
+server.send(sms_message)
+
+# Гипотеза 1: В переменной нет прогноза погоды для Курска
+# Способ проверки: Выведу переменную new_event
+# Код для проверки: print(new_event)
+# Установленный факт: ...
+# Вывод: ...
+
+# Гипотеза 2.1: town_title на самом деле пуста
+# Способ проверки: ...
+# Код для проверки: ...
+# Установленный факт: ...
+# Вывод: ...
+
+# Гипотеза 2.2: В town_title не название города
+# Способ проверки: ...
+# Код для проверки: ...
+# Установленный факт: ...
+# Вывод: ...
